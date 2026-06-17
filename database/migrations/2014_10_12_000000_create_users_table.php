784bbaa29ceff3fa2fa164403968810b5b3f01cd
tom <abc@gmail.com>
2026-06-17 16:51:40 +0400

    设计用户表完成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->comment('昵称');
+            $table->string('email')->unique()->comment('邮件地址');
+            $table->string('phone')->unique()->comment('手机号码');
+            $table->string('avatar')->nullable()->comment('头像');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        $user1 = User::findOrFail(1);
+        $user1->name = '超级管理员';
+        $user1->phone = '13521009090';
+        $user1->save();
+        $user2 = User::findOrFail(2);
+        $user2->name = '喜马拉雅';
+        $user2->phone = '13521009091';
+        $user2->save();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
